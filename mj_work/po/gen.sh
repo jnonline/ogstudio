f83ae3281918e7e1bcc2cf565a4c6265853fb3a9
kornerr <kornerr@7257fc52-512e-0410-984f-c3a75a44aa05>
2011-05-10 10:15:22 +0000

    fixes for German

--- mj_work/po/gen.sh
+++ mj_work/po/gen.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-msgfmt ru.po -o ../../mj_res/locale/ru/LC_MESSAGES/OGSMahjong.mo
+for file in $(ls *.po); do
+    fileLen=${#file}
+    fileLang=${file:0:fileLen-3}
+    dir="../../mj_res/locale/$fileLang/LC_MESSAGES"
+    mkdir -p $dir
+    msgfmt $file -o $dir/OGSMahjong.mo
+done
+
