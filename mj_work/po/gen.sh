8494c846a33ebd75006834c109927473226d7171
kornerr <kornerr@7257fc52-512e-0410-984f-c3a75a44aa05>
2011-07-23 12:06:19 +0000

    ditched old gettext translations

--- mj_work/po/gen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for file in $(ls *.po); do
-    fileLen=${#file}
-    fileLang=${file:0:fileLen-3}
-    dir="../../mj_res/locale/$fileLang/LC_MESSAGES"
-    mkdir -p $dir
-    msgfmt $file -o $dir/OGSMahjong.mo
-done
-
