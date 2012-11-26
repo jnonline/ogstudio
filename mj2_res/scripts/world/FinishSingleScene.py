b1c196ce230465ad0b2f1555762917c30b7e194c
kornerr <kornerr@7257fc52-512e-0410-984f-c3a75a44aa05>
2012-11-26 13:13:33 +0000

    Added Start/FinishSingleScene for mj scene, removed some world stub listeners

---
+++ mj2_res/scripts/world/FinishSingleScene.py
@@ -0,0 +1,6 @@
+
+def run(listener):
+    world = listener.parent
+    if (len(world.scenes) == 1):
+        print "There's only one Scene found in the World. Finishing it."
+        world.finishScene(world.scenes.keys()[0])
--- mj2_res/scripts/world/onFinish.py
+++
@@ -1,6 +0,0 @@
-
-
-def run(listener):
-    print "We're exiting. This is onFinish handler :)"
-    #commonFuncs.printDate()
-    #print "Current time: ", time.asctime(time.localtime(time.time()))
