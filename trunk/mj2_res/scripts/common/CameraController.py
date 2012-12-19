6df333fd2de06b091719b9266c6e77e7cf8c2890
kornerr <kornerr@7257fc52-512e-0410-984f-c3a75a44aa05>
2012-12-19 14:40:16 +0000

    GamePlayer now makes scene visible

--- trunk/mj2_res/scripts/common/CameraController.py
+++ trunk/mj2_res/scripts/common/CameraController.py
@@ -2,17 +2,17 @@
 Camera controller
 """
 
-import mjin2
+import pymjin2
 
-class CameraController(mjin2.InputListener):
+class CameraController(pymjin2.InputListener):
     def __init__(self, camera):
         mjin2.InputListener.__init__(self)
         self.camera = camera
-        self.rotation = mjin2.Vec3(90, 0, 0)
+        self.rotation = pymjin2.Vec3(90, 0, 0)
         self.x = 0
         self.y = 0
     def onInput(self, e):
-        if (e.press and e.input == mjin2.INPUT_MOUSE_MOVE):
+        if (e.press and e.input == pymjin2.INPUT_MOUSE_MOVE):
             dx = e.x - self.x
             dy = e.y - self.y
             self.rotation.x += dx
--- trunk/mj2_res/scripts/world/FinishSingleScene.py
+++ trunk/mj2_res/scripts/world/FinishSingleScene.py
@@ -4,3 +4,4 @@ def run(listener):
     if (len(world.scenes) == 1):
         print "There's only one Scene found in the World. Finishing it."
         world.finishScene(world.scenes.keys()[0])
+        world.player.setScene(None)
