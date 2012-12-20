53d5279e2cf5ccc3797e892628c07bcc5025f26b
kornerr <kornerr@7257fc52-512e-0410-984f-c3a75a44aa05>
2012-12-20 08:02:48 +0000

    CameraController works fine now

--- trunk/mj2_res/scripts/common/CameraController.py
+++ trunk/mj2_res/scripts/common/CameraController.py
@@ -9,17 +9,22 @@ class CameraController(pymjin2.InputListener):
         pymjin2.InputListener.__init__(self)
         self.camera = None
         self.rotation = pymjin2.Vec3(90, 0, 0)
-        self.x = 0
-        self.y = 0
+        self.x = None
+        self.y = None
         self.speed = 0.5
     def onInput(self, e):
-        if (self.camera and e.press and e.input == pymjin2.INPUT_MOUSE_MOVE):
+        if (self.x == None and self.y == None):
+            self.x = e.x
+            self.y = e.y
+        elif (self.camera and e.press and e.input == pymjin2.INPUT_MOUSE_MOVE):
             dx = e.x - self.x
             dy = e.y - self.y
             dx *= self.speed
             dy *= self.speed
-            self.rotation.x += dx
-            self.rotation.y += dy
+            print "dx: {0} dy : {1}".format(dx, dy)
+            self.rotation.z -= dx
+            self.rotation.x -= dy
+            print "rotation: {0} {1} {2}".format(self.rotation.x, self.rotation.y, self.rotation.z)
             self.camera.setRotation(pymjin2.degreeToQuaternion(self.rotation))
             self.x = e.x
             self.y = e.y
