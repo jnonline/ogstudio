9c05763201527fd4cd70fdea02af2d57dfc70f2b
Kai.Saerthen.Darker@Gmail.com <Kai.Saerthen.Darker@Gmail.com@7257fc52-512e-0410-984f-c3a75a44aa05>
2013-03-02 11:27:23 +0000

    removed old prototype

--- shuan_work/Prototype/Modules/Core/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-'''
-Shuan gameplay slice prototype core mechanics
-
-(c) 2010 Opensource Game Studio Team (http://opengamestudio.org)
-'''
--- shuan_work/Prototype/Modules/EnemyWeapons/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-'''
-Shuan gameplay slice prototype enemy weapons
-
-(c) 2010 Opensource Game Studio Team (http://opengamestudio.org)
-'''
